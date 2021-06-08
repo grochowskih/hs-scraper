231fc6e8404cc0e9e73ee1fe3b42f657fcb707a2
grochowskih <grochowskih@gmail.com>
2021-06-08 20:43:53 +0200

    Add db connection (without data)

--- ScraperProject/app/Main.hs
+++ ScraperProject/app/Main.hs
@@ -4,14 +4,16 @@ module Main where
 
 import Backend.Scraper
 import Prelude
+import Backend.DAO
 
 main :: IO ()
-main = do
-    putStrLn "Cena od: "
-    priceFrom <- getLine
-    putStrLn "Cena do: "
-    priceTo <- getLine 
-    res <- titles (Just priceFrom) (Just priceTo) Nothing Nothing Nothing Nothing
-    case res of 
-        Just toShow -> print toShow
-        Nothing -> undefined
+--main = do
+--    putStrLn "Cena od: "
+--    priceFrom <- getLine
+--    putStrLn "Cena do: "
+--    priceTo <- getLine 
+--    res <- titles (Just priceFrom) (Just priceTo) Nothing Nothing Nothing Nothing
+--    case res of 
+--        Just toShow -> print toShow
+--        Nothing -> undefined
+main = testConnect
--- ScraperProject/src/Backend/DAO.hs
+++ ScraperProject/src/Backend/DAO.hs
@@ -1,3 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Backend.DAO where
 
-import Database.MySQL.simple
+import Database.MySQL.Base
+    ( query_,
+      connect,
+      defaultConnectInfo,
+      ConnectInfo(ciHost, ciPort, ciUser, ciPassword, ciDatabase) )
+import System.IO.Streams as Streams
+
+testConnect :: IO () 
+testConnect = do
+    conn <- Database.MySQL.Base.connect
+        defaultConnectInfo {ciHost = "sql11.freesqldatabase.com", ciPort = 3306, ciUser = "sql11417464", ciPassword = "XDMEcWeuRp", ciDatabase = "sql11417464"}
+    (defs, is) <- query_ conn "SELECT * FROM offers"
+    print =<< Streams.toList is
